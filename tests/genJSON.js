96e2c3bc6d2656bc81d7f6ff8d44a1b149f73e89
Eric Prud'hommeaux <eric@w3.org>
2015-09-09 01:25:05 -0400

    ~ changed namespace to sht

--- tests/genJSON.js
+++ tests/genJSON.js
@@ -19,7 +19,7 @@ var P = {
   "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
   "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
   "mf": "http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#",
-  "shext":   "http://www.w3.org/ns/shextest#"
+  "sht":   "http://www.w3.org/ns/sht#"
 };
 
 var apparentBase = __dirname + "/manifest";
@@ -36,12 +36,14 @@ parser.parse(
   });
 
 function genText () {
-  var g = [];
+  var g = []; // stuff everything into a JSON-LD @graph
   var ret = {
     "@context": "https://raw.githubusercontent.com/shexSpec/test-suite/gh-pages/tests/manifest-context.jsonld",
     "@graph": g
   };
+
   store.addPrefixes(P);
+
   var manifest = store.find(null, "rdf:type", "mf:Manifest")[0].subject;
   var manifestComment = util.getLiteralValue(store.find(manifest, "rdfs:comment", null)[0].object);
   var entries = [];
@@ -50,9 +52,14 @@ function genText () {
     entries.push(store.find(head, "rdf:first", null)[0].object);
     head = store.find(head, "rdf:rest", null)[0].object;
   }
+  var unmatched = entries.reduce(function (ret, ent) {
+    ret[ent] = true;
+    return ret;
+  }, {});
   var expectedTypes = ["NotValid", "PositiveSyntax", "Valid", "ValidationTest", "ValidationFailure"].map(function (suffix) {
-    return P.shext + suffix;
+    return P.sht + suffix;
   });
+
   g.push({
     "@id": "http://shexspec.github.io/test-suite/tests/manifest.ttl",
     "@type": "mf:Manifest",
@@ -68,7 +75,11 @@ function genText () {
       return [t.subject, t.object];
     }).filter(function (t) {
       var ret = entries.indexOf(t[0].substr(stripPath, 999)) !== -1;
-      if (ret === false) { console.warn("unreferenced test: " + t[0]); }
+      if (ret === false) {
+        console.warn("unreferenced test: " + t[0]);
+      } else {
+        delete unmatched[t[0].substr(stripPath, 999)];
+      }
       return ret;
     }).sort(function (l, r) {
       return entries.indexOf(l[0].substr(stripPath, 999)) >
@@ -76,23 +87,27 @@ function genText () {
     }).map(function (st) {
       var s = st[0], t = st[1];
       return [
-        //      ["rdf"  , "type"    , function (v) { return v.substr(P.shext.length); }],
+        //      ["rdf"  , "type"    , function (v) { return v.substr(P.sht.length); }],
         ["mf"   , "name"    , function (v) { return util.getLiteralValue(v); }],
         ["rdfs" , "comment" , function (v) { return util.getLiteralValue(v); }],
-        ["shext", "status"  , function (v) { return "mf:"+v.substr(P.mf.length); }],
-        ["shext", "schema"  , function (v) { return v.substr(stripPath-8); }],
-        ["shext", "shape"   , function (v) { return v.indexOf(__dirname) === 0 ? v.substr(__dirname.length+1) : v; }],
-        ["shext", "data"    , function (v) { return v.substr(stripPath-8); }],
-        ["shext", "focus"   , function (v) { return v.indexOf(__dirname) === 0 ? v.substr(__dirname.length+1) : v; }],
-        ["shext", "result"  , function (v) { return v.substr(stripPath-8); }]
+        ["sht", "status"  , function (v) { return "mf:"+v.substr(P.mf.length); }],
+        ["sht", "schema"  , function (v) { return v.substr(stripPath-8); }],
+        ["sht", "shape"   , function (v) { return v.indexOf(__dirname) === 0 ? v.substr(__dirname.length+1) : v; }],
+        ["sht", "data"    , function (v) { return v.substr(stripPath-8); }],
+        ["sht", "focus"   , function (v) { return v.indexOf(__dirname) === 0 ? v.substr(__dirname.length+1) : v; }],
+        ["sht", "result"  , function (v) { return v.substr(stripPath-8); }]
       ].reduce(function (ret, row) {
         var found = store.findByIRI(s, P[row[0]]+row[1], null);
         // console.log(found);
         if (found.length)
           ret[row[1]] = row[2](found[0].object);
         return ret;
-      }, {"@id": s.substr(stripPath), "@type": "shext:"+t.substr(P.shext.length)});
+      }, {"@id": s.substr(stripPath), "@type": "sht:"+t.substr(P.sht.length)});
     })
   });
+  var remaining = Object.keys(unmatched);
+  if (remaining.length) {
+    console.warn("no definitions for " + remaining.join(", "));
+  }
   console.log(JSON.stringify(ret, null, "  "));
 }
